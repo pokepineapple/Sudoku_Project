dd1f67fb0ca8c3f17cff68f4cd3fbfd07f8ca3d2
pokepineapple <atcutuli25@gmail.com>
2022-03-02 15:25:44 -0800

    Update Sudoku.py
    Modified to make code more readable.
    however, this is a failed code as fixing the issues would be a complicated mess.
    Restarted from scratch in another file.

--- Sudoku.py
+++ Sudoku.py
@@ -3,63 +3,86 @@ from matplotlib.style import use
 import numpy as np
 import random as rn
 
+#Modified to make it more simplistic/readable
+#However, the main issue lies with the problem of 
+#conflicting numbers and checking if a number 
+#needs to be used in X row or Y column would produce
+#a multitude of issues leaving a 0 beahind,
+#so this code will be left as it is
 
 def number_list():
     numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
     return numbers
 
-#Checks if number can be used at location by checking
-#Row, Column, and box to see if the number exists
-def valid_location(matrix, row, col, number, banned):
-    #matrix2 = np.transpose(matrix)
-    #checks already used numbers in the 3x3 box
-    if number in banned:
-        return False
-    elif number in matrix[row]:
-        return False
-    elif number in np.transpose(matrix)[col]:
-        return False
-    else:
-        return True
+#True = Number has been Used
+#False = Number not been Used
+def used_row(matrix, row, number):
+    for col in np.arange(0, len(matrix[0])):
+        if matrix[row][col] == number:
+            return True
+    return False
 
-#Insert [1:9] values into 3 by 3 box in the matrix
+def used_col(matrix, col, number):
+    for row in np.arange(0,len(matrix[0])):
+        if matrix[row][col] == number:
+            return True
+    return False
+
+def used_box(matrix, row, col, number):
+    for r in np.arange(row, row+3):
+        for c in np.arange(col, col+3):
+            if matrix[r][c] == number:
+                return True
+    return False
+
+def must_use_number():
+    return
+
 def number_box(matrix, row, col):
+    #Skips over the Boxes that were filled diagonally
+    if matrix[row][col] != 0:
+        return matrix
+    
     num_list = number_list()
-    banned = []
     rn.shuffle(num_list)
-    for r in np.arange(row,row+3):              #Iterate Rows
-        for c in np.arange(col, col+3):         #Iterate Columns
-            #print("AVAILABLE NUMBERS", unused_num)
-            for number in num_list:             #Iterate through 1 to 9
-                #Check if the number for [r,c] position is allowed
-                if valid_location(matrix, r, c, number, banned) == True:
-                    #Stores the approve number
+        
+    for r in np.arange(row,row+3):              
+        for c in np.arange(col, col+3):  
+            for number in num_list:
+                if (used_box(matrix, row, col, number) or 
+                    used_col(matrix, c, number) or 
+                    used_row(matrix, r, number)) == False:
                     matrix[r][c] = number
-                    #Keeps track of used numbers
-                    banned.append(number)
+                    
                 if matrix[r][c] != 0:
                     break
-
-    #CHeck if the number_box has any 0s at all
-    #if yes, reset the box to 0 and rerun function
-    #else nothing happens
-                
     return matrix
 
+#Fill 3 boxes diagonally as they do not influence each other
+def fill_dia(matrix):
+    col = 0
+    for row in np.arange(0,9,3):
+        list = number_list()
+        rn.shuffle(list)
+        for r in np.arange(row, row+3):              
+            for c in np.arange(col, col+3):
+                number = rn.choice(list)
+                matrix[r][c] = number
+                list.remove(number)
+        col += 3
+    return matrix
 
-#Creates a 9x9 Sudoku puzzle
+#Remember to change it back to 9
 def puzzle_generate():
     puzzle = np.zeros((9,9))
+    fill_dia(puzzle)
+    
     for i in np.arange(0,9,3):
         for j in np.arange(0,9,3):
             number_box(puzzle, i, j)
+    
     return puzzle
     
     
 sudoku = puzzle_generate()
 for line in sudoku: print(line)
-
-# Tested Box functionality for 3x3 grind
-#Still need to test it in a 9x9 grid
-# box = np.zeros((3,3))
-# print(number_box(box, 0, 0))
