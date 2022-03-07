62e039be3e21db637f980c7ab27beb3b5055663e
pokepineapple <atcutuli25@gmail.com>
2022-03-07 13:25:34 -0800

    Modifications and Additions made
    modified Sudoku_Final to comment out test code and make changes in sudoku_generator(). Made more development in Sudoku GUI's display for sudoku. Need to work on displaying the sudoku board now and iterate the numbers when clicking on button

--- Sudoku_Final.py
+++ Sudoku_Final.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python3 
+# Python3 version used: 3.9.10
+from itertools import count
+import random as rn
+import numpy as np
+
 # pattern for a baseline valid solution
 # determines location value that is determined to be
 # a valid number solution for its location on the grid
@@ -5,7 +11,6 @@ def pattern(row,col, base, dim):
     return (base*(row%base)+row//base+col)%dim
 
 #randomize rows, columns and numbers (of valid base pattern)
-import random as rn
 def randomize(s): 
     return rn.sample(s,len(s)) 
 
@@ -17,7 +22,7 @@ def rc_pattern(rbase, base):
             result.append(i*base + j)
     return result
 
-def sudoku_generator():    
+def solution_generator():    
     base  = 3 #Alter if want a bigger board
     dim   = base*base
     rBase = range(base) 
@@ -28,7 +33,48 @@ def sudoku_generator():
     #nested for loops to produce the board with
     #no duplicated numbers in each row, col, and box
     puzzle = [ [nums[pattern(r,c, base, dim)] for r in rows] for c in cols ]
-    return puzzle
+    return puzzle #np.asarray(puzzle) 
 
-# solution = sudoku_generator()
+def remove_number(matrix, number):
+    counter = number
+    copy = matrix.copy()
+    while counter != 0:
+        for r in range(9):
+            for c in range(9): 
+                if copy[r][c] != 0:
+                    if rn.randint(0, 2) == 0 and counter > 0:
+                        copy[r][c] = 0
+                        counter -= 1
+        if counter == 0:
+            break
+    return copy
+
+#NOTE: Wierd Bullshit occurred when using .copy() on the sol causing
+#      variable overwrite elsewhere, but using np.copy() prevents this
+#      so funct temporarily converts list to ndarray then back to list
+def sudoku_generator(sol, level):
+    solu = np.copy(sol)
+    if level == 1:   #Easy Mode
+        return np.ndarray.tolist(remove_number(solu, rn.randint(33,43)))
+    elif level == 2: #Normal Mode
+        return np.ndarray.tolist(remove_number(solu, rn.randint(44,53)))
+    elif level == 3: #Hard Mode
+        return np.ndarray.tolist(remove_number(solu, rn.randint(54,60)))
+    elif level == 4: #Random Mode
+        return np.ndarray.tolist(remove_number(solu, rn.randint(33,60)))
+    else: #Precaution for anything odd
+        return 0
+
+
+#Test Code
+# solution = solution_generator()
+# puzzle = sudoku_generator(np.copy(solution), 2)
+
+# print(type(solution))
+# print(type(puzzle))
+
+
+# print("Puzzle")
+# for line in puzzle: print(line)
+# print("Solution")
 # for line in solution: print(line)
--- Sudoku_GUI.py
+++ Sudoku_GUI.py
@@ -1,26 +1,54 @@
+from msilib import text
+from turtle import color
+import numpy as np
 import tkinter as tk
-from Sudoku_Final import sudoku_generator
+from Sudoku_Final import *
 
-#window = tk.Tk()
+game = tk.Tk()  
+game.title("Sudoku")  
+game.geometry("900x550")  
 
+sol = []     #Stores the solution
+sudoku = []       #Stores the base puzzle keep to keep for resets
+sudoku_edit = []  #Copies the puzzle to allow edits
 
-#solution = sudoku_generator()
-# for line in solution: print(line)s
+#setup for 1 slot on the grid
+def display_button(xpos, ypos):
+    frame = tk.Frame(game, width=56, height=56, highlightbackground = "black", 
+                         highlightthickness = 2, bd = 0) #their units in pixels
+    button1 = tk.Button(frame, text="Clicky")
+    frame.grid_propagate(False) #disables resizing of frame
+    frame.columnconfigure(0, weight=1) #enables button to fill frame
+    frame.rowconfigure(0,weight=1) #any positive number would do the trick
+    frame.place(x = xpos, y = ypos) #put frame where the button should be
+    button1.grid(sticky="wens")
+    
 
+def display_sudoku(level):
+    global sol, sudoku
+    #Checks if theres a existing puzzle, and removes everything if True
+    if sol != []:
+        sol.clear()
+        sudoku.clear()
+        
+    sol = solution_generator()
+    sudoku   = sudoku_generator(sol, level)
+    
+    # #create a loop that creates a button
+    for x in range(375, 879, 56):
+        for y in range(25,529, 56):
+            display_button(x, y)
 
-obj=tk.Tk()  
-obj.title("c# corner")  
-obj.geometry("500x500")  
+#bttn = tk.Button(button_border, text = 'Submit', fg = 'black',
+#                 bg = 'yellow',font = (("Times New Roman"),15))
 
+easy = tk.Button(game, text = "Easy", height = 2, width= 10, command = display_sudoku(1)).place(x = 50, y = 150)
+normal = tk.Button(game, text = "Normal", height = 2, width= 10, command = display_sudoku(2)).place(x = 50, y = 200)
+hard = tk.Button(game, text = "Hard", height = 2, width= 10, command = display_sudoku(3)).place(x = 50, y = 250)
+random = tk.Button(game, text = "Random", height = 2, width= 10, command = display_sudoku(4)).place(x = 50, y = 300)
 
-exit_button = tk.Button(
-    obj,
-    text='Button Orgasm',
-    command=lambda: obj.quit()
-)
+# center = tk.Frame(game, bg='pink', highlightbackground="blue", highlightthickness=2, 
+#                 width=504, height=504, padx=3, pady=3)
+#center.place(x = 375, y = 25)
 
-exit_button.pack(ipadx=5,ipady=5,expand=True)
-
-#wintext = tk.Text(obj)  
-#wintext.pack()  
-obj.mainloop()  
+game.mainloop()  
