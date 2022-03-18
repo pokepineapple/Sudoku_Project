330c24f177de32611cde92bc293f5a50bbf1f641
pokepineapple <atcutuli25@gmail.com>
2022-03-18 13:11:33 -0700

    Update Sudoku_GUI.py
    implemented a reset button to clear the sudoku board. Working on Submit and Hint functions. AI Solver will be last.

--- Sudoku_GUI.py
+++ Sudoku_GUI.py
@@ -8,12 +8,17 @@ game = tk.Tk()
 game.title("Sudoku")  
 game.geometry("900x550")  
 
-sol = []     #Stores the solution
-sudoku = []       #Stores the base puzzle keep to keep for resets
+game['background'] = "purple"
+
+sol = []            #Stores the solution
+sudoku = []         #Stores the base puzzle keep to keep for resets
 player_sudoku = []  #Copies the puzzle to allow edits
 
+button_list = [[0 for x in range(9)] for y in range(9)]     #Stores array of the buttons to access it
+
+
 def display_button(xpos, ypos):
-    global player_sudoku, sudoku
+    global player_sudoku, sudoku, button_list
     
     #SCREAMS
     def update_text(button, x,y):
@@ -29,14 +34,18 @@ def display_button(xpos, ypos):
     X = int((xpos - 375)/56)
     Y = int((ypos - 25)/56)
     
-    frame = tk.Frame(game, width=56, height=56, highlightbackground = "black", bg = "pink",
+    frame = tk.Frame(game, width=56, height=56, highlightbackground = "black", 
                          highlightthickness = 2, bd = 0) 
     #add command connecting to text_button()
     if sudoku[X][Y] != 0:
-        button1 = tk.Button(frame, text= sudoku[X][Y],font = (("Arial"),20)) 
+        button1 = tk.Button(frame, text= sudoku[X][Y],font = (("Arial"),24), relief= tk.FLAT, bg = "lime green") 
+        
+        button_list[X][Y] = 0
     else:
-        button1 = tk.Button(frame, text= " ", font = (("Kristen ITC"),20)) 
+        button1 = tk.Button(frame, text= " ", font = (("Kristen ITC"),24)) 
         button1.config(command= lambda: update_text(button1, X, Y))
+        
+        button_list[X][Y] = button1 #Do i even need to?
         
     frame.grid_propagate(False)         #disables resizing of frame
     frame.columnconfigure(0, weight=1)  #enables button to fill frame
@@ -60,6 +69,27 @@ def display_sudoku(level):
         for y in range(25,529, 56):
             display_button(x, y)
 
+def hint_button():
+    
+    return
+
+def submit_button():
+    global sol, player_sudoku
+    if sol == player_sudoku:  #if the arrays are a match
+        return
+    else: #if they are not a match
+        return
+
+def reset_button():
+    global button_list, player_sudoku, sudoku
+    for i in range(0,9):
+        for j in range(0,9):
+            if sudoku[i][j] == 0:
+                player_sudoku[i][j] = 0  #resets what ever number stored to 0
+                reset_butt = button_list[i][j]
+                reset_butt['text'] = " "
+                button_list[i][j] = reset_butt
+    
 #bttn = tk.Button(button_border, text = 'Submit', fg = 'black',
 #                 bg = 'yellow',font = (("Times New Roman"),15))
 
@@ -67,5 +97,10 @@ easy = tk.Button(game, text = "Easy", height = 2, width= 10, command = lambda: d
 normal = tk.Button(game, text = "Normal", height = 2, width= 10, command = lambda: display_sudoku(2)).place(x = 50, y = 200)
 hard = tk.Button(game, text = "Hard", height = 2, width= 10, command = lambda: display_sudoku(3)).place(x = 50, y = 250)
 random = tk.Button(game, text = "Random", height = 2, width= 10, command = lambda: display_sudoku(4)).place(x = 50, y = 300)
+
+AI = tk.Button(game, text = "AI Solver", height = 2, width= 10).place(x = 150, y = 150)
+hint = tk.Button(game, text = "Hint", height = 2, width= 10).place(x = 150, y = 200)
+reset = tk.Button(game, text = "Reset", height = 2, width= 10,command = lambda: reset_button()).place(x = 150, y = 250)
+submit = tk.Button(game, text = "Submit", height = 2, width= 10).place(x = 150, y = 300)
 
 game.mainloop()  
